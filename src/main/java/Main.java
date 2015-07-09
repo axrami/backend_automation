66daf004fbd6cae8197509a6be6f2f5152fe154f
Andrew <andrew@aramirez2-mac.local>
2015-07-09 15:33:56 -0700

    runnable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,19 @@ import service.Visit;
 /**
  * Created by andrew on 6/16/15.
  */
-public class Main {
-    public static void main(String args[]) {
-        int x = 0;
+public class Main implements Runnable {
 
+    public void run() {
+        int x = 0;
         while(x < 10) {
             Visit visit = new Visit();
             visit.launch();
             x++;
         }
+    }
 
+    public static void main(String args[]) {
+        (new Thread(new Main())).start();
 
     }
 }
